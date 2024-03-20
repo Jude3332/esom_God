389d0cc887f7a715e598eb72dd1b022d070549ac
Girrona <162429867+Girrona@users.noreply.github.com>
2024-03-21 00:07:34 +0100

    nectar test

---
+++ components/Carousel/Content.jsx
@@ -0,0 +1,38 @@
+export const images = [
+    {
+        id: 1,
+        src: "/caroimages/editSol.png",
+        title: "Solidity Language",
+        description: "Solidity is a high-level programming language used for writing smart contracts on blockchain platforms, particularly Ethereum. It is statically typed and supports inheritance, libraries, and complex user-defined types. "
+    },
+    {
+        id: 2,
+        src: "/caroimages/editPy.png",
+        title: "Python Language",
+        description: "Python is a powerful and user-friendly programming language known for its simplicity, readability, and versatility. It supports multiple programming paradigms, including procedural, object-oriented, and functional programming."
+    },
+    {
+        id: 3,
+        src: "/caroimages/editJs.png",
+        title: "Javascript Langauage",
+        description: "JavaScript is a versatile and widely-used programming language primarily known for its role in web development. It is a high-level, interpreted language that is dynamically typed and supports object-oriented, imperative, and functional programming paradigms."
+    },
+    {
+        id: 4,
+        src: "/caroimages/editC.png",
+        title: "C# Language",
+        description: "C# is a modern, object-oriented programming language developed by Microsoft. It is part of the .NET framework and is used to build a wide range of applications, including desktop applications, web applications, games (with Unity game engine), and enterprise software."
+    },
+    // {
+    //     id: 5,
+    //     src: "/caroimages/html33.png",
+    //     title: "Graphics design",
+    //     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type"
+    // },
+    // {
+    //     id: 6,
+    //     src: "/caroimages/butt.jpeg",
+    //     title: "Game Development",
+    //     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type"
+    // }
+]
